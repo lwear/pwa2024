799f82b9a11787c76af8ffaf0d9f814f7d8b0b53
lwear <33907570+lwear@users.noreply.github.com>
2024-10-01 14:32:21 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icons/horse-192-192.png',
+        '/icons/horse-48-48.png',
+        '/icons/horse-72-72.png',
+        '/icons/horse-96-96.png',
+        '/icons/horse-144-144.png',
+        '/icons/horse-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
